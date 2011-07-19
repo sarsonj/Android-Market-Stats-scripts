c54e4b4cc4c992be0337664401d76c49d90c0060
Jindrich Sarson <jindra@tappytaps.com>
2011-07-19 18:23:27 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+apps = {
+            "com.tappytaps.android.babymonitoralarm.full": "Baby Monitor Alarm",
+            "com.tappytaps.android.geotagphotospro": "Geotag Photos Pro",
+            "com.tappytaps.android.enumbers.full": "E Numbers Calc"
+            }
